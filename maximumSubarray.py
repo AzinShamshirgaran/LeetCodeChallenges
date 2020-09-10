a9376b8dcc6a9fb7cd0e2ae8a07b37f136da0fdf
Azin Shamshirgaran <azinsh1988@gmail.com>
2020-09-09 23:27:11 -0400

    string slicing

--- maximumSubarray.py
+++ maximumSubarray.py
@@ -11,6 +11,20 @@ class Solution:
 
         return sum
 
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         if len(nums) == 1:
+#             return nums[0]
+#         sum = nums[0]
+#         sum_total = nums[0]
+#         for i in range (1, len(nums)):
+#             if sum + nums[i] > nums[i]:
+#                 sum = sum + nums[i]
+#             else:
+#                 sum = nums[i]
+#             sum_total = max( sum_total, sum)
+#         return sum_total
+
 if __name__ == '__main__':
     commands = [4,-1,4,-2,4]
 
